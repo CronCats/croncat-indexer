ba149921668b2bca417bb72db0e39923462d234a
Zack Kollar <rallokkcaz@gmail.com>
2022-10-11 23:56:26 -0700

    :tada: Refactor indexer/mod.rs to have better naming for models and crate values

--- src/indexer/mod.rs
+++ src/indexer/mod.rs
@@ -1,30 +1,45 @@
 use color_eyre::{eyre::eyre, Result};
-
 use sea_orm::entity::prelude::*;
 use sea_orm::Set;
 
-pub use model::{prelude::*, *};
+// Sane aliases
+use crate::block::Block;
+use model::block::ActiveModel as BlockModel;
 
+// Tell clippy to ignore the generated model code.
 #[allow(clippy::all)]
 mod model;
 
-pub async fn index_block(db: &DatabaseConnection, block: crate::block::Block) -> Result<()> {
-    let height: i64 = block.header().height.into();
-    let chain_id = block.header().chain_id.to_string();
-    let time = block.header().time.to_rfc3339();
-    let time = DateTime::parse_from_str(time.as_str(), "%Y-%m-%dT%H:%M:%S%.f%Z")?;
-    let hash = block.header().hash().to_string();
-    let num_txs = block.data().as_ref().iter().count() as i64;
+///
+/// Create a block database entry from a block.
+///
+impl From<Block> for BlockModel {
+    fn from(block: Block) -> Self {
+        let height: i64 = block.header().height.into();
+        let chain_id = block.header().chain_id.to_string();
+        let time = DateTime::parse_from_str(
+            block.header().time.to_rfc3339().as_str(),
+            "%Y-%m-%dT%H:%M:%S%.f%Z",
+        )
+        .unwrap();
+        let hash = block.header().hash().to_string();
+        let num_txs = block.data().as_ref().iter().count() as i64;
 
-    let block = block::ActiveModel {
-        height: Set(height),
-        chain_id: Set(chain_id),
-        time: Set(time),
-        hash: Set(hash),
-        num_txs: Set(num_txs), // TODO: Probably don't need this field.
-    };
+        Self {
+            height: Set(height),
+            chain_id: Set(chain_id),
+            time: Set(time),
+            hash: Set(hash),
+            num_txs: Set(num_txs),
+        }
+    }
+}
 
-    block
+///
+/// Index a block into the database.
+///
+pub async fn index_block(db: &DatabaseConnection, block: Block) -> Result<()> {
+    BlockModel::from(block)
         .insert(db)
         .await
         .map_err(|e| eyre!("Failed to insert block: {}", e))?;
